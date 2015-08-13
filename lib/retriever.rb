1b29d9033c0479b1f11b637c07ad3e961aec25b3
Jacob Smergel <jacob.smergel@gmail.com>
2015-08-13 11:27:56 -0400

    rebuild gem with class names

---
+++ lib/retriever.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'json'
+
+module Dribbble
+  class Retriever
+    def dribbble
+      uri = URI("http://api.dribbble.com/shots/everyone")
+      Net::HTTP.get(uri)
+    end
+
+    def dribbbled
+      JSON.parse(dribbble)["shots"].map { |x| {title: x["title"], image_url: x["url"], likes_count: x["likes_count"]} }
+    end
+
+    def drib_image
+      JSON.parse(dribbble)["shots"].map { |x| {image_url: x["image_url"]} }
+    end
+  end 
+end
+
+
